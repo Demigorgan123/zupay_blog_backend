ba67d3c5c207a2b1ea33a198b42a173de23372ec
Demigorgan123 <rbanswal123@gmail.com>
2024-08-21 02:12:39 +0530

    - install mongoose - install dotenv - connect MongoDB Atlas

---
+++ connectDB.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+const connectDB = async()=>{
+    return mongoose.connect(process.env.DB_URL)
+}
+
+module.exports = {
+    connectDB
+}

6e996a526541f4502f4f9221e4daee9719897667
Demigorgan123 <rbanswal123@gmail.com>
2024-08-21 02:46:25 +0530

    install express - create server - create routes

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const express = require('express')
+const app = express();
+const postRouter = require('./routes/post')
 const {connectDB} = require('./connectDB')
+const PORT = 5000
 
 connectDB().then(()=>console.log("Database Connected"))
+
+app.use('/post', postRouter)
+
+app.listen(PORT, ()=>console.log(`Listening on port: ${PORT}`));
